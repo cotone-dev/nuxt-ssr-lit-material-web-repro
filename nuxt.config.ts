5a03f4d82b08de4248a2adc5ca81beed357f9fdf
cotone <151821279+cotone-dev@users.noreply.github.com>
2025-09-04 09:36:09 +0900

    chore: #1 upgrade packages (#2)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2025-09-04',
   devtools: { enabled: false },
   modules: [['nuxt-ssr-lit', { litElementPrefix: ['md-'] }]],
   nitro: {
